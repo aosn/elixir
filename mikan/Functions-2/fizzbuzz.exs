4151e5f2d7143e6ed855afdbe78135135dfbf30d
mikan <yutakakato@icloud.com>
2017-03-12 23:04:33 +0900

    add Functions-3 and fix Functions-2

--- mikan/Functions-2/fizzbuzz.exs
+++ mikan/Functions-2/fizzbuzz.exs
@@ -2,7 +2,9 @@ fizzbuzz = fn
 (0,0,_) -> "FizzBuzz"
 (0,_,_) -> "Fizz"
 (_,0,_) -> "Buzz"
+(_,_,z) -> z
 end
 IO.puts(fizzbuzz.(0,0,1)) # FizzBuzz
 IO.puts(fizzbuzz.(0,1,0)) # Fizz
 IO.puts(fizzbuzz.(1,0,0)) # Buzz
+IO.puts(fizzbuzz.(1,1,1)) # 1
