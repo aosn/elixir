1c00b06f8d84f8223308d61ff2f438928e15ec74
tomono <tomono@tomono-mbp13.local>
2017-03-12 23:13:08 +0900

    add 'IO.puts' in functions_3.exs

--- intptr-t/Functions-3/functions_3.exs
+++ intptr-t/Functions-3/functions_3.exs
@@ -4,10 +4,10 @@ fizz_buzz = fn
   n -> fizz_buzz_echo.(rem(n, 3), rem(n, 5), n)
 end
 
-fizz_buzz.(10) #=> "Buzz"
-fizz_buzz.(11) #=> 11
-fizz_buzz.(12) #=> "Fizz"
-fizz_buzz.(13) #=> 13
-fizz_buzz.(14) #=> 14
-fizz_buzz.(15) #=> "FizzBuzz"
-fizz_buzz.(16) #=> 16
+IO.puts fizz_buzz.(10) #=> "Buzz"
+IO.puts fizz_buzz.(11) #=> 11
+IO.puts fizz_buzz.(12) #=> "Fizz"
+IO.puts fizz_buzz.(13) #=> 13
+IO.puts fizz_buzz.(14) #=> 14
+IO.puts fizz_buzz.(15) #=> "FizzBuzz"
+IO.puts fizz_buzz.(16) #=> 16
