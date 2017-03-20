304a9b3581f2f92ccec1810203783327bfc5f6bf
Takumi Okamoto <takumi1988okamoto@gmail.com>
2017-03-20 21:29:05 +0900

    Fiz->Fizzに修正

--- mrbearing/ch5/functions2.exs
+++ mrbearing/ch5/functions2.exs
@@ -6,7 +6,7 @@
 
 fizz_buzz = fn
     0, 0, _ -> "FizzBuzz"
-    0, _, _ -> "Fiz"
+    0, _, _ -> "Fizz"
     _, 0 ,_ -> "Buzz"
     _, _, c -> c
 end
--- mrbearing/ch5/functions3.exs
+++ mrbearing/ch5/functions3.exs
@@ -1,6 +1,6 @@
 say_fizz_buzz = fn
     0, 0, _ -> "FizzBuzz"
-    0, _, _ -> "Fiz"
+    0, _, _ -> "Fizz"
     _, 0 ,_ -> "Buzz"
     _, _, c -> c
 end
