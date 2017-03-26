050d3554c6dc866236bb988c65a83eb17b18be06
Takumi Okamoto <takumi1988okamoto@gmail.com>
2017-03-26 23:03:21 +0900

    ch6-6

--- mrbearing/memo/ch6.exs
+++ mrbearing/memo/ch6.exs
@@ -1,4 +1,18 @@
-defmodule Functional do 
-    def of(0), do : 1
-    def of(n), do : n * of(n-1)
-end
+# defmodule Functional do 
+#     def of(0), do : 1
+#     def of(n), do : n * of(n-1)
+# end
+
+# defmodule Guard do 
+#     def what_is(x) when is_number(x) do
+#         IO.puts "#{x} is a number"
+#     end
+#     def what_is(x) when is_list(x) do
+#         IO.puts "#{x} is a list"
+#     end 
+#     def what_is(x) when is_atom(x) do
+#         IO.puts "#{x} is atom"
+#     end
+# end
+
+
