b13e7a448d3dc19b99864d96fd443a4fde4bd748
mikan <yutakakato@icloud.com>
2017-03-26 21:25:39 +0900

    update ModulesAndFunctions-3

--- mikan/ModulesAndFunctions-3/quadruple.exs
+++ mikan/ModulesAndFunctions-3/quadruple.exs
@@ -1,7 +1,7 @@
 defmodule Times do
   def double(n), do: n * 2
   def triple(n), do: n * 3
-  def quadruple(n), do: double(n) + double(n)
+  def quadruple(n), do: double(double(n))
 end
 IO.puts Times.quadruple(0) # 0
 IO.puts Times.quadruple(1) # 4
