74b8536f868c0a32d713ee68c688d4f59bffb369
mikan <yutakakato@icloud.com>
2017-03-27 00:07:18 +0900

    add ModulesAndFunctions-6

--- test/mikan/ModulesAndFunctions-4/sum_test.exs
+++ test/mikan/ModulesAndFunctions-4/sum_test.exs
@@ -1,11 +1,13 @@
 defmodule SumTest do
   use ExUnit.Case
-  test "1 case" do
-    assert Sum.sum(1) == 1
+  test "0 case" do
+    assert Sum.sum(0) == 0
   end
   test "normal case" do
+    assert Sum.sum(1) == 1
     assert Sum.sum(2) == 1 + 2
     assert Sum.sum(3) == 1 + 2 + 3
     assert Sum.sum(4) == 1 + 2 + 3 + 4
+    assert Sum.sum(10) == 55
   end
 end
