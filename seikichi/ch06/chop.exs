65a53fa04f6afbebf1146800f5c6c59d36ae5f08
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-03-26 22:48:09 +0900

    Refactor

--- seikichi/ch06/chop.exs
+++ seikichi/ch06/chop.exs
@@ -1,16 +1,16 @@
 defmodule Chop do
   def guess(actual, left..right) do
-    guess_helper(actual, div(left + right, 2), left..right)
+    helper(actual, div(left + right, 2), left..right)
     |> Enum.each(&(IO.puts "Is it #{&1}"))
   end
 
-  def guess_helper(actual, guess, _) when actual === guess do
+  def helper(actual, guess, _) when actual === guess do
     [guess]
   end
-  def guess_helper(actual, guess, left.._) when actual < guess do
-    [guess] ++ guess_helper(actual, div(left + guess, 2), left..guess)
+  def helper(actual, guess, left.._) when actual < guess do
+    [guess | helper(actual, div(left + guess, 2), left..guess)]
   end
-  def guess_helper(actual, guess, _..right) when actual > guess do
-    [guess] ++ guess_helper(actual, div(guess + right, 2), guess..right)
+  def helper(actual, guess, _..right) when actual > guess do
+    [guess | helper(actual, div(guess + right, 2), guess..right)]
   end
 end
