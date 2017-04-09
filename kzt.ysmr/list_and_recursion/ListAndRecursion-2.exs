c99213da6829951b182c7dae02fcaba810b6c0a4
kzt-ysmr <kzt.ysmr@gmail.com>
2017-04-09 23:28:10 +0900

    modified: ListAndRecursion-2

--- kzt.ysmr/list_and_recursion/ListAndRecursion-2.exs
+++ kzt.ysmr/list_and_recursion/ListAndRecursion-2.exs
@@ -1,7 +1,9 @@
 
 defmodule MyList do
-    def max( [] ), do: 0
-    def max( [ head | tail ] ), do: _max( head, max( tail ) )
+    def max( list ), do: reduce( list, 0, fn ( x, y ) -> _max( x, y ) end )
+
+    def reduce( [], default, op ), do: default
+    def reduce( [ head | tail ], default, op ), do: op.( head, reduce( tail, default, op ) )
 
     defp _max( x, y ) when x < y, do: y
     defp _max( x, y ), do: x
@@ -9,5 +11,4 @@ end
 
 IO.puts MyList.max( [] )
 IO.puts MyList.max( [ 1, 2, 3, 1 ] )
-
 
