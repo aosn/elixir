b8aa014cad298957ab355e9a0b2e1dff2053994c
akeboshi <akari.akeboshi@gmail.com>
2017-04-23 21:35:13 +0900

    fix 7-4

--- akeboshi/ch07/ListAndRecursion-4.exs
+++ akeboshi/ch07/ListAndRecursion-4.exs
@@ -1,8 +1,7 @@
 defmodule MyList do
-  def span(from, to), do: _span(from, to)
-  defp _span(from, to) when from > to, do: _span(to, from)
-  defp _span(from, to) when from == to, do: [to]
-  defp _span(from, to), do: [from | _span(from + 1, to)]
+  def span(from, to) when from > to, do: span(to, from)
+  def span(from, to) when from == to, do: [to]
+  def span(from, to), do: [from | span(from + 1, to)]
 end
 
 IO.inspect("MyList.span(1,3) #=> [1,2,3]")
