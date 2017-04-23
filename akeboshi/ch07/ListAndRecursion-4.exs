6d34adc10bedf34b17087c6121c823aa354bde31
akeboshi <akari.akeboshi@gmail.com>
2017-04-23 21:43:12 +0900

    fix 7-4

--- akeboshi/ch07/ListAndRecursion-4.exs
+++ akeboshi/ch07/ListAndRecursion-4.exs
@@ -2,6 +2,7 @@ defmodule MyList do
   def span(from, to) when from > to, do: span(to, from)
   def span(from, to) when from == to, do: [to]
   def span(from, to), do: [from | span(from + 1, to)]
+  def span_e(from, to), do: Enum.to_list from..to
 end
 
 IO.inspect("MyList.span(1,3) #=> [1,2,3]")
@@ -11,3 +12,9 @@ IO.inspect(MyList.span(3,1))
 IO.inspect("MyList.span(1,1) #=> [1]")
 IO.inspect(MyList.span(1,1))
 
+IO.inspect("MyList.span_e(1,3) #=> [1,2,3]")
+IO.inspect(MyList.span_e(1,3))
+IO.inspect("MyList.span_e(3,1) #=> [3,2,1]")
+IO.inspect(MyList.span_e(3,1))
+IO.inspect("MyList.span_e(1,1) #=> [1]")
+IO.inspect(MyList.span_e(1,1))
