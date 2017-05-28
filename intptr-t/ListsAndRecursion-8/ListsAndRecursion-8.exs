1a5c659bec91c317ca114ad3a7a9d05b7be7b1c4
intptr-t <niwatho.twi@gmail.com>
2017-05-28 22:51:12 +0900

    use total_amount

--- intptr-t/ListsAndRecursion-8/ListsAndRecursion-8.exs
+++ intptr-t/ListsAndRecursion-8/ListsAndRecursion-8.exs
@@ -8,7 +8,8 @@ defmodule TaxOrder do
       [
         id: order[:id],
         ship_to: order[:ship_to],
-        net_amount: (1.0+tax) * order[:net_amount]
+        net_amount: order[:net_amount],
+        total_amount: (1.0+tax) * order[:net_amount]
       ]
     end
   end
@@ -28,11 +29,11 @@ orders = [
 
 IO.inspect TaxOrder.calc_orders_with_tax(orders, tax_rates)
 
-#[[id: 123, ship_to: :NC, net_amount: 107.5],
-# [id: 124, ship_to: :OK, net_amount: 35.5],
-# [id: 125, ship_to: :TX, net_amount: 25.92],
-# [id: 126, ship_to: :TX, net_amount: 48.384],
-# [id: 127, ship_to: :NC, net_amount: 26.875],
-# [id: 128, ship_to: :MA, net_amount: 10.0],
-# [id: 129, ship_to: :CA, net_amount: 102.0],
-# [id: 120, ship_to: :NC, net_amount: 53.75]]
+#[[id: 123, ship_to: :NC, net_amount: 100.0, total_amount: 107.5],
+# [id: 124, ship_to: :OK, net_amount: 35.5, total_amount: 35.5],
+# [id: 125, ship_to: :TX, net_amount: 24.0, total_amount: 25.92],
+# [id: 126, ship_to: :TX, net_amount: 44.8, total_amount: 48.384],
+# [id: 127, ship_to: :NC, net_amount: 25.0, total_amount: 26.875],
+# [id: 128, ship_to: :MA, net_amount: 10.0, total_amount: 10.0],
+# [id: 129, ship_to: :CA, net_amount: 102.0, total_amount: 102.0],
+# [id: 120, ship_to: :NC, net_amount: 50.0, total_amount: 53.75]]
