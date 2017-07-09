355e41dc8f59eeda5352ba11275adc7870dfef40
LagunaPresa <sunspotactivity@gmail.com>
2017-07-09 22:15:36 +0900

    Add ControlFlow-1

---
+++ lagunapresa/s12/ControlFlow-1.exs
@@ -0,0 +1,16 @@
+defmodule FizzBuzz do
+  def upto(n) when n > 0, do: 1..n |> Enum.map(&fizzbuzz/1)
+
+  defp fizzbuzz(n) do
+    case { rem(n, 3), rem(n, 5) } do
+      {0, 0} -> "FizzBuzz"
+      {0, _} -> "Fizz"
+      {_, 0} -> "Buzz"
+      _      -> n
+    end
+  end
+end
+
+# iex(2)> FizzBuzz.upto(20)
+# [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14,
+#  "FizzBuzz", 16, 17, "Fizz", 19, "Buzz"]
