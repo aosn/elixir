9f01504ccc5004ade86c2770ba110db4a18fa2f4
Takumi Okamoto <takumi1988okamoto@gmail.com>
2017-07-16 21:41:32 +0900

    add

---
+++ mrbearing/lib/ch12/ControlFlow1.exs
@@ -0,0 +1,22 @@
+
+defmodule FizzBuzz do
+    
+    def upto(n) when n > 0 do
+        1..n |>Enum.map(&fizzbuzz/1) 
+    end
+
+    defp fizzbuzz(n) do
+        case {rem(n,3)==0 , rem(n,5)==0} do
+            {true,true} -> "FizzBuzz"  
+            {true,false} -> "Fizz"  
+            {false, true} -> "Buzz"
+            _ -> n
+        end  
+    end
+
+    
+end
+
+
+
+IO.puts FizzBuzz.upto(100)
