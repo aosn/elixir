47ed9d4ad535b4c2ac9b964ef36f1f383309ff18
LagunaPresa <sunspotactivity@gmail.com>
2017-10-29 22:03:09 +0900

    add opts-4

--- lagunapresa/s16/sequence/lib/sequence/server.ex
+++ lagunapresa/s16/sequence/lib/sequence/server.ex
@@ -1,8 +1,25 @@
 # OPT-Servers-1 implemented
 # OPT-Servers-2 implemented
+# OPT-Servers-4 implemented
 
 defmodule Sequence.Server do
   use GenServer
+
+  ### APIs ###
+
+  def start_link(initial_state) do
+    GenServer.start_link(__MODULE__, initial_state, name: __MODULE__)
+  end
+
+  def push(value) do
+    GenServer.cast(__MODULE__, {:push, value})
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  ### Implementations ###
 
   def handle_cast({:push, value}, current_stack) do
     { :noreply, [value|current_stack] }
@@ -37,3 +54,19 @@ end
 # {:ok, #PID<0.157.0>}
 # iex(1)> GenServer.cast(:seq, {:push, 1})
 # :ok
+
+
+# OPT-Servers-4 tried
+
+# iex(1)> Sequence.Server.start_link([1])
+# {:ok, #PID<0.123.0>}
+# iex(2)> Sequence.Server.push(2)
+# :ok
+# iex(3)> Sequence.Server.push(3)
+# :ok
+# iex(4)> Sequence.Server.pop
+# 3
+# iex(5)> Sequence.Server.pop
+# 2
+# iex(6)> Sequence.Server.pop
+# 1
