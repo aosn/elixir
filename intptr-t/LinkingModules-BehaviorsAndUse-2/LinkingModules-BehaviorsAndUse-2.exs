56097dcc0b7460ec646baeb647b6194fb7106958
intptr-t <niwatho.twi@gmail.com>
2017-12-18 01:55:05 +0900

    rename module name

---
+++ intptr-t/LinkingModules-BehaviorsAndUse-2/LinkingModules-BehaviorsAndUse-2.exs
@@ -0,0 +1,42 @@
+# iex(1)> IO.ANSI.format(["hello, ", :inverse, :bright, "world!"], true)
+# [[[[[[], "hello, "] | "\e[7m"] | "\e[1m"], "world!"] | "\e[0m"]
+
+defmodule Tracer do
+  def dump_args(args) do 
+    args |> Enum.map(&inspect/1) |> Enum.join(", ")
+  end
+
+  def dump_defn(name, args) do 
+    "#{name}(#{dump_args(args)})"
+  end
+
+  defmacro def(definition={name,_,args}, do: content) do 
+    quote do
+      Kernel.def(unquote(definition)) do
+        call_text = "#{Tracer.dump_defn(unquote(name), unquote(args))}"
+        IO.puts IO.ANSI.format([:inverse, "==>call:    #{call_text}"])
+        result = unquote(content)
+        IO.puts IO.ANSI.format([:bright, "<== result: #{result}"])
+        result
+      end
+    end
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Kernel, except: [def: 2]
+      import unquote(__MODULE__), only: [def: 2]
+    end
+  end  
+end
+
+defmodule Test do
+  import Kernel, except: [def: 2]
+  import Tracer, only:   [def: 2]
+
+  def puts_sum_three(a,b,c),  do: IO.inspect(a+b+c)
+  def add_list(list),         do: Enum.reduce(list, 0, &(&1+&2))
+end
+
+Test.puts_sum_three(1, 2, 3)
+Test.add_list([5, 6, 7, 8])
--- intptr-t/LinkingModules-BehaviorsAndUse-2/LinkingModules-BehaviorsAndUse.exs
+++
@@ -1,4 +0,0 @@
-IO.puts IO.ANSI.format(["hello, ", :inverse, :bright, "world!"], true)
-
-# iex(1)> IO.ANSI.format(["hello, ", :inverse, :bright, "world!"], true)
-# [[[[[[], "hello, "] | "\e[7m"] | "\e[1m"], "world!"] | "\e[0m"]
